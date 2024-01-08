a0a08670f9cef6d4ae91813191cb03a41317bd43
José Á. Calero M. <joseacalerom@gmail.com>
2024-01-08 14:49:50 +0100

    Revisión de estilos básicos

--- weather-web/src/App.jsx
+++ weather-web/src/App.jsx
@@ -1,10 +1,10 @@
-import "./App.scss";
+import styles from "./App.module.scss";
 import { CityInput } from "./components/CityInput/CityInput";
 
 function App() {
   return (
     <>
-      <div className="appContainer">
+      <div className={styles.appContainer}>
         <h1>Weather web</h1>
         <CityInput />
       </div>
