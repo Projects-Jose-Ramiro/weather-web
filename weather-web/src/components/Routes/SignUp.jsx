b92436d5125f4b3eb6ce5a3a9630254543dfdea1
Radadeli <ramirodelillo@gmail.com>
2024-03-21 21:41:01 +0100

    feat: advance with backend

--- weather-web/src/components/Routes/SignUp.jsx
+++ weather-web/src/components/Routes/SignUp.jsx
@@ -1,12 +1,47 @@
 import { useState } from "react";
+import useAuth from "../../Auth/useAuth";
+import { Navigate } from "react-router";
+
 
 export function SignUp() {
   const [name, setName] = useState("")
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+
+  const auth = useAuth
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    try{
+      const response = await fetch(`${API_URL}/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          username,
+          email,
+          password
+        })
+      })
+      if(response.ok){
+        console.log("User created successfully")
+      } else {
+        console.log("Something went wrong")
+      }
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+  if (auth.isAuthenticated) {
+    return <Navigate to="/dashboard" />
+  }
+
   return (
-    <form className="formLogin">
+    <form className="formLogin" onSubmit={handleSubmit}>
       <h1>SignUp</h1>
       <label>Name</label>
       <input
