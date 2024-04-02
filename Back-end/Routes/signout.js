30bf44a4ac4117075d31be19b96ab9a1743de11d
Radadeli <ramirodelillo@gmail.com>
2024-04-02 11:22:02 +0200

    signOut button logic

--- Back-end/Routes/signout.js
+++ Back-end/Routes/signout.js
@@ -1,8 +1,23 @@
+const getTokenFromHeader = require("../auth/getTokenFromHeader");
+const { jsonResponse } = require("../lib/jsonResponse");
+const Token = require("../schema/token");
 
 
 const router = require("express").Router();
 
-router.post("/", (req, res) => {
+router.delete("/", async (req, res) => {
+try {
+    const refreshToken = getTokenFromHeader(req.headers)
 
+    if(refreshToken) {
+        await Token.findOneAndRemove({ token: refreshToken});
+        res.status(200).json(jsonResponse(200, { message: "Token deleted" }));
+    } else {
+        res.status(400).json(jsonResponse(400, { message: "Refresh token not provided" }));
+      }
+} catch (error) {
+    console.log(error);
+    res.status(500).json(jsonResponse(500, { message: "Server error"}))
+}
 })
 module.exports = router;
