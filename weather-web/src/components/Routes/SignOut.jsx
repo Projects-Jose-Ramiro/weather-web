311c15e985169fdd449667b6922e9e098a19f07d
Radadeli <ramirodelillo@gmail.com>
2024-04-02 12:20:47 +0200

    signout button corrections

--- weather-web/src/components/Routes/SignOut.jsx
+++ weather-web/src/components/Routes/SignOut.jsx
@@ -1,18 +1,33 @@
 // SignOut.jsx
 import { useAuth } from "../../Auth/AuthProvider";
 import { useNavigate } from "react-router"
+import { API_URL } from "../../Auth/constants";
 
 export function SignOut() {
   const goTo = useNavigate()
-  const { signOut } = useAuth();
+  const auth = useAuth();
 
-  const handleSignOut = () => {
-    signOut();
-    console.log("SignOut successfully")
-    goTo("/")
-  };
+  async function handleSignOut (e) {
+   e.preventDefault();
+    try {
+      const response = await fetch(`${API_URL}/signout`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${auth.getRefreshToken()}`, 
+        },
+      })
+      if(response.ok) {
+        auth.signOut();
+        goTo("/")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <button onClick={handleSignOut}>Sign Out</button>
   );
+
 }
